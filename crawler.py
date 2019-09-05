9012d7d0b68f564b836b2a433db50042b4cdcf95
lohhans <lohansdemelo1108@gmail.com>
2019-09-05 17:53:40 -0300

    Ideia inicial

---
+++ crawler.py
@@ -0,0 +1,10 @@
+# Exemplo a ser buscado:
+#
+# <blockquote><p>Every next level of your life will demand a different you. <a
+# class="clicktotweet" title="Click to tweet!" href="https://twitter.com/intent/tweet?text=Every next level of your
+# life will demand a different you.+http://bit.ly/mostinspirationalquotes" target="_blank" rel="nofollow noopener
+# noreferrer">Click to tweet</a></p></blockquote>
+
+# TODO: Retirar o nome do autor
+# TODO: Salvar cada frase em um txt
+# Obs: Para retirar o nome do autor, a ideia eh dar um split no primeiro ".", porem algumas frases tem mais de um "."!
